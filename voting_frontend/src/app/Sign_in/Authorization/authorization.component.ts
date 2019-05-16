f1819aa798aec0ffdce9de31e954995c0b59b5e8
Eugen <xdantex00008@gmail.com>
2019-05-16 14:09:05 +0300

    Merge

--- voting_frontend/src/app/Sign_in/Authorization/authorization.component.ts
+++ voting_frontend/src/app/Sign_in/Authorization/authorization.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {LocalStorageService} from '../../services/local-storage.service';
-import {Local} from "protractor/built/driverProviders";
+import {ToasterService} from 'angular2-toaster';
 
 class User {
   public id: number;
@@ -19,12 +19,13 @@ class User {
 
 export class AuthorizationComponent implements OnInit {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private toaster: ToasterService) { }
 
   authorize(login: string, password: string): void {
     this.httpClient.get<number>(`/authorization/${login}_${password}`)
-      .subscribe(res => {LocalStorageService.setUserId(res); alert('User autorized'); },
-        error => {LocalStorageService.setUserId(1); alert('User not found'); });
+      .subscribe(res => {LocalStorageService.setUserId(res);  this.toaster.pop('success', 'Sign in succesfuly'); },
+        error => {LocalStorageService.setUserId(1); this.toaster.pop('error', 'User not found'); });
 
   }
   check(): void {
